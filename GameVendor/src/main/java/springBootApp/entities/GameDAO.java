94a9d2bcf62b3f498ed697becabd386c8708f6b2
daniel-reich <Daniel@Daniels-MacBook-Pro-4.local>
2016-12-14 10:13:22 -0700

    first commit

---
+++ GameVendor/src/main/java/springBootApp/entities/GameDAO.java
@@ -0,0 +1,20 @@
+package springBootApp.entities;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+public interface GameDAO extends CrudRepository<Game, Long> {
+
+    List<Game> findByCategoryId(long categoryId);
+
+    List<Game> findByVendorId(long vendorId);
+
+    @Query("SELECT g FROM Game g WHERE g.gameTitle LIKE ?1%")
+    List<Game> findByNameStartsWith(String name);
+
+
+}
